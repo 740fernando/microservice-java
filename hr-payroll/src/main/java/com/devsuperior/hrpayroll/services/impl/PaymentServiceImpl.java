25b59d785a50a932272d1c54c1898e8eb345d6ee
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-04-04 17:31:14 -0300

    Feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/impl/PaymentServiceImpl.java
@@ -1,41 +1,22 @@
 package com.devsuperior.hrpayroll.services.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 import com.devsuperior.hrpayroll.model.Payment;
-import com.devsuperior.hrpayroll.model.dto.WorkerResponseDTO;
 import com.devsuperior.hrpayroll.services.PaymentService;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
-	public static final String PATH_WORKERS_ID = "/workers/{id}";
-
-	private static final String NO_SPACE = "";
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 
 	@Override
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();// Utilizado como parametro do metodo getForObject, o map
-															// contem <String, String> pq os parametros no http são
-															// sempre string
-		uriVariables.put("id", NO_SPACE.valueOf(workerId)); // id = id do worker
-
-		WorkerResponseDTO worker = restTemplate.getForObject(workerHost.concat(PATH_WORKERS_ID),
-				WorkerResponseDTO.class, uriVariables);
-		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-
+			var worker = workerFeignClient.findById(workerId).getBody();
+			return new Payment(worker.getName(),worker.getDailyIncome(),days);
 	}
 }
