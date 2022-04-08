7e810f7a4144270edede01a1907d3b3c81a18b75
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-04-08 17:07:19 -0300

    Added exception and handler

--- hr-user/src/main/java/com/devsuperior/hruser/service/impl/UserServiceImpl.java
+++ hr-user/src/main/java/com/devsuperior/hruser/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.devsuperior.hruser.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.hruser.exception.UserException;
 import com.devsuperior.hruser.model.User;
 import com.devsuperior.hruser.repositories.UserRepository;
 import com.devsuperior.hruser.service.UserService;
@@ -15,11 +16,11 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public User findByEmail(String email) {
-		return repo.findByEmail(email);
+		return repo.findByEmail(email).orElseThrow(()-> new UserException(email));
 	}
 	@Override
 	public User findById(Long id) {
-		return repo.findById(id).orElseThrow(()-> new RuntimeException());
+		return repo.findById(id).orElseThrow(()-> new UserException(id));
 	}
 
 }
