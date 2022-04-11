28b6f44c7a707afb8a27318ca74e8b26ff3e4f1c
Fernando <fernando.luiz92@hotmail.com>
2022-04-11 06:41:22 -0300

    Worker -  Finished exceptions and handlers

--- hr-worker/src/main/java/com/devsuperior/hrworker/service/impl/WorkerServiceImpl.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/service/impl/WorkerServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.hrworker.exception.WorkerException;
 import com.devsuperior.hrworker.model.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
 import com.devsuperior.hrworker.service.WorkerService;
@@ -24,7 +25,7 @@ public class WorkerServiceImpl implements WorkerService {
 	@Override
 	public Worker findById(Long id) {
 		Optional<Worker> objFound = repository.findById(id);
-		return objFound.orElseThrow(()-> new RuntimeException());
+		return objFound.orElseThrow(()-> new WorkerException(id));
 	}
 
 }
