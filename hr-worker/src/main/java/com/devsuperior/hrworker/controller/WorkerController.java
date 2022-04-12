2a3da0d190a23f8c4c279edbd985a8d29ed12e94
Fernando <fernando.luiz92@hotmail.com>
2022-04-12 12:50:10 -0300

    hr-worker Postgres dev profile

--- hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.hrworker.mapper.WorkerMapper;
-import com.devsuperior.hrworker.model.Worker;
 import com.devsuperior.hrworker.model.dto.WorkerResponseDTO;
 import com.devsuperior.hrworker.service.WorkerService;
 
